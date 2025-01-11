0cbf52dd57bd8d44004fdbaa63e206c832b6696b
Afsar <afsar_zan@yahoo.com>
2025-01-10 17:50:49 -0800

    Generate meta data for custom component

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,8 +1,19 @@
-export default async function productDetails({params}: {
-    params: Promise<{ productId: string}>
-}) {
+import { Metadata } from "next";
+
+type Props  = {
+    params:Promise<{ productId: string}>;
+}
+
+export const generateMetadata = async ( {
+    params,
+}: Props): Promise<Metadata> => {
+    const id = (await params).productId;
+    return {
+        title: `product id is ${id}`
+    }
+}
+
+export default async function productDetails({params}: Props) {
     const productId = (await params).productId;
-    return (
-        <div>details about product {productId}</div>
-    );
+    return (  <div>details about product {productId}</div>);
 }
