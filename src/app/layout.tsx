69f5cd4d92c15faad1d71486afcc92958762c225
Afsar <afsar_zan@yahoo.com>
2025-01-13 00:45:37 -0800

    Link using router from next navigations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,8 @@
 
 import "./styles.css";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+// import { usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 // export const metadata = {
 //  title:{
@@ -19,8 +20,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
 
-  const pathname = usePathname();
-  console.log({pathname})
+  // const pathname = usePathname();
+  const router = useRouter();
+  const handleClick = () => {
+    router.push('/products')
+  }
+
   return (
     <html lang="en">
       <body>
@@ -45,39 +50,9 @@ export default function RootLayout({
                 >
                   Get started
                 </a>
-                <button
-                  data-collapse-toggle="mobile-menu-2"
-                  type="button"
-                  className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                  aria-controls="mobile-menu-2"
-                  aria-expanded="false"
-                >
-                  <span className="sr-only">Open main menu</span>
-                  <svg
-                    className="w-6 h-6"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                      clip-rule="evenodd"
-                    ></path>
-                  </svg>
-                  <svg
-                    className="hidden w-6 h-6"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                      clip-rule="evenodd"
-                    ></path>
-                  </svg>
-                </button>
+               <button onClick={handleClick}>
+                Go to product page
+               </button>
               </div>
               <div
                 className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
@@ -94,8 +69,9 @@ export default function RootLayout({
                     </a>
                   </li>
                   <li>
-                    <Link href="/about"  className={`${ pathname === "/about" ? "text-yellow-300" : "" } block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0lg:dark:hover:bg-transparent dark:border-gray-700`}
-                    >About </Link>
+                    <Link href="/about"></Link>
+                    {/* <Link href="/about"  className={`${ pathname === "/about" ? "text-yellow-300" : "" } block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0lg:dark:hover:bg-transparent dark:border-gray-700`}
+                    >About </Link> */}
                   </li>
                   <li>
                   <Link href="/products" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
